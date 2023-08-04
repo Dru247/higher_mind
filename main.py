f35c65194e1547a7279814ee28f38907d8832cf0
Dru <lextand@gmail.com>
2023-08-04 17:19:07 +0300

    add main.py

---
+++ main.py
@@ -0,0 +1,155 @@
+# import datetime
+import os
+import sqlite3 as sq
+import telebot
+# import threading
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+TOKEN = os.getenv('TELEGRAM_TOKEN')
+my_id = os.getenv('TELEGRAM_MY_ID')
+bot = telebot.TeleBot(TOKEN)
+database = "main.db"
+bot_commands = [
+    '/commands',
+    '/create_task',
+    '/list_tasks',
+    '/task_completed'
+]
+
+def create_db():
+    with sq.connect(database) as con:
+        cur = con.cursor()
+        cur.execute("""CREATE TABLE IF NOT EXISTS tasks (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            id_user INTEGER,
+            task TEXT,
+            status INTEGER
+            )""")
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет! Для создания задачи введи /create_task. Для отображения задач введи /list_tasks')
+
+
+@bot.message_handler(commands=['commands'])
+def list_commands(message):
+    for bot_command in bot_commands:
+        bot.send_message(message.chat.id, bot_command)
+
+
+@bot.message_handler(commands=['create_task'])
+def reminder_message(message):
+    if message.chat.id == my_id:
+        bot.send_message(message.chat.id, 'Введи текст задачи')
+        bot.register_next_step_handler(message, set_task)
+    else:
+        bot.send_message(message.chat.id, 'Сорян, бот для избранных ;)')
+
+
+def set_task(message):
+    with sq.connect(database) as con:
+        cur = con.cursor()
+        cur.execute(f"INSERT INTO tasks (id_user, task, status) VALUES ({message.chat.id}, '{message.text}', {1})")
+
+
+@bot.message_handler(commands=['list_tasks'])
+def list_task_request(message):
+        bot.send_message(message.chat.id, 'Введи New или Old')
+        bot.register_next_step_handler(message, list_tasks)
+
+
+def list_tasks(message):
+    data_records = {
+        "Old": 0,
+        "New": 1
+    }
+    try:
+        with sq.connect(database) as con:
+            cur = con.cursor()
+            cur.execute(f"SELECT * FROM tasks WHERE id_user = {message.chat.id} AND status = {data_records[message.text]}")
+            for record in cur:
+                bot.send_message(message.chat.id, f"{record[0]}: {record[2]}")
+    except:
+        bot.send_message(message.chat.id, 'Некорректно')
+
+
+@bot.message_handler(commands=['task_completed'])
+def task_completed(message):
+    bot.send_message(message.chat.id, 'Введи ID задачи')
+    bot.register_next_step_handler(message, change_task)
+
+
+def change_task(message):
+    try:
+        with sq.connect(database) as con:
+            cur = con.cursor()
+            cur.execute(f"UPDATE tasks SET status = {0} WHERE id = {int(message.text)}")
+        bot.send_message(message.chat.id, f"Задача с ID {message.text} выполнена")
+    except:
+        bot.send_message(message.chat.id, 'ID не верен')
+
+
+# @bot.message_handler(commands=['/list_completed'])
+# def list_task(message):
+#     with sq.connect(database) as con:
+#         cur = con.cursor()
+#         cur.execute(f"SELECT * FROM tasks WHERE id_user = {message.chat.id} AND status = {1}")
+#         for record in cur:
+#             bot.send_message(message.chat.id, f"{record[0]}: {record[2]}")
+
+
+
+# def list_completed_tasks(message):
+#     print(0)
+#     with sq.connect(database) as con:
+#         cur = con.cursor()
+#         print(1)
+#         cur.execute(f"SELECT * FROM tasks WHERE status = {0}")
+#         print(2)
+#         for record in cur:
+#             bot.send_message(message.chat.id, f"{record[0]}: {record[2]}")
+
+
+# @bot.message_handler(commands=['reminder'])
+# def reminder_message(message):
+#     bot.send_message(message.chat.id, 'Введите текст напоминания')
+#     bot.register_next_step_handler(message, set_reminder_name)
+
+
+# def set_reminder_name(message):
+#     user_data = {}
+#     user_data[message.chat.id] = {'reminder_name': message.text}
+#     bot.send_message(message.chat.id, 'Введите дату и время, когда хотите получить напоминание в формате ГГГГ-ММ-ДД чч:мм:сс')
+#     bot.register_next_step_handler(message, reminder_set, user_data)
+
+# def reminder_set(message, user_data):
+#     try:
+#         reminder_time = datetime.datetime.strptime(message.text, '%Y-%m-%d %H:%M:%S')
+#         now = datetime.datetime.now()
+#         delta = reminder_time - now
+#         if delta.total_seconds() <= 0:
+#             bot.send_message(message.chat.id, 'Вы ввели прошедшую дату, попробуйте ещё раз')
+#         else:
+#             reminder_name = user_data[message.chat.id]['reminder_name']
+#             bot.send_message(message.chat.id, 'Напоминание {} установлено на {}.'.format(reminder_name, reminder_time))
+#             reminder_time = threading.Timer(delta.total_seconds(), send_reminder, [message.chat.id, reminder_name])
+#             reminder_time.start()
+#     except ValueError:
+#         bot.send_message(message.chat.id, 'Вы ввели неверный формат даты и времени, попробуйте ещё раз')
+
+# def send_reminder(chat_id, reminder_name):
+#     bot.send_message(chat_id, 'Время получить ваше напоминание "{}"!'.format(reminder_name))
+
+
+# @bot.message_handler(func=lambda message: True)
+# def handler_all_message(message):
+#     bot.send_message(message.chat.id, 'Я не понимаю, что вы говорите. Чтобы сосздать напоминие введите /reminder')
+
+if __name__ == '__main__':
+    create_db()
+    bot.polling(none_stop=True)
