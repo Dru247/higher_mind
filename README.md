44b816da80eb6351d38f53f8494c30acf184b5f7
Dru <lextand@gmail.com>
2023-08-23 16:29:45 +0300

    add search-module

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # higher_mind
+
+Обновляем пакеты:<br>
+**sudo apt update && sudo apt upgrade**<br>
+---
+Установка необходимых для корректной работы google-chroma пакетов:<br>
+**sudo apt install -y libxss1 libappindicator1 libindicator7**<br>
+---
+Скачиваем Chrome:<br>
+**sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb**<br>
+Устанавливаем Chrome:<br>
+**sudo dpkg -i google-chrome*.deb**<br>
+---
+Фиксим/подтягиваем зависимости:<br>
+**sudo apt install -y -f**<br>
+---
+Устанавливаем виртуальное окружение:<br>
+**python3 -m venv .venv**<br>
+Запускаем виртуальное окружение:<br>
+**source .venv/bin/activate**<br>
+Обновляем pip:<br>
+**pip install -U pip**<br>
+Устанавливаем библиотеки:<br>
+**pip install -r requirements.txt**<br>
+---
+Создаём файл **.env**<br>
+Заносим переменные:<br>
+**TELEGRAM_TOKEN**<br>
+**TELEGRAM_MY_ID**<br>
+**EMAIL**<br>
+**EMAIL_PASSWORD**<br>
+**EMAIL_RECIPIENT**<br>
+**SEARCH_URL**<br>
+**SEARCH**<br>
+
--- main.py
+++ main.py
@@ -1,15 +1,35 @@
 import os
+import random
+import re
+import smtplib
 import schedule
 import sqlite3 as sq
 import telebot
 import threading
 import time
 
+from bs4 import BeautifulSoup
 from dotenv import load_dotenv
+from email.mime.text import MIMEText
 from pytz import timezone
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 load_dotenv()
+
+options = webdriver.ChromeOptions()
+
+options.add_argument("--no-sandbox")
+
+# for ChromeDriver version 79.0.3945.16 or over
+options.add_argument("--disable-blink-features=AutomationControlled")
+
+# background mode
+options.add_argument("--headless")
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
 TOKEN = os.getenv('TELEGRAM_TOKEN')
 my_id = os.getenv('TELEGRAM_MY_ID')
@@ -107,6 +127,174 @@ def make_reminder(message):
             time.sleep(1)
     
     threading.Thread(target=timer, args=(message,)).start()
+
+
+@bot.message_handler(commands=['search'])
+def task_completed(message):
+    bot.send_message(message.chat.id, 'Старт поиска')
+    search_people()
+    bot.send_message(message.chat.id, 'Поиск закончен')
+
+
+def search_people():
+    search_db = "people.db"
+    search_url = os.getenv('SEARCH_URL')
+
+    def create_db():
+        with sq.connect(search_db) as con:
+            cur = con.cursor()
+            cur.execute("DROP TABLE IF EXISTS people")
+            cur.execute("""CREATE TABLE IF NOT EXISTS people (
+                id INTEGER PRIMARY KEY,
+                place TEXT,
+                name TEXT,
+                age INTEGER,
+                hands INTEGER,
+                height INTEGER,
+                weight INTEGER,
+                al TEXT,
+                price INTEGER
+                )""")
+
+
+    def get_source_html(url):
+
+        with sq.connect(search_db) as con:
+            cur = con.cursor()
+
+            try:
+                driver.get(url)
+                time.sleep(random.randint(0,3))
+                html = driver.page_source
+                soup = BeautifulSoup(html, "lxml")
+                count_lists = soup.find("div", class_="pagingList").find_all("a")
+                count = int(count_lists[-2].text)
+
+                for i in range(count+1):
+                    url_new = re.split('index=', url, maxsplit=0)
+                    url_new_2 = f'{url_new[0]}index={i}{url_new[1][1:]}'
+                    driver.get(url_new_2)
+                    time.sleep(random.randint(0,10))
+                    html = driver.page_source
+                    soup = BeautifulSoup(html, "lxml")        
+                    cards = soup.find_all("div", class_="adp_edin_ank")
+                    
+                    for card in cards:
+                        id = card.find("a", class_="metro_title_link").get("href")
+                        id_new = int(re.sub(r'\D', '', id))
+                        place = card.find("a", class_="metro_title").text
+                        name = card.find("a", class_="metro_title_link").text
+                        name_new = re.sub(r'\W', '', name)
+                        age = card.find("div", class_="txtparam").find("span")
+                        age_new = int(re.sub(r'\D', '', str(age)))
+                        hands = card.find("div", class_="txtparam").find("span").find_next_sibling().find_next_sibling()
+                        hands_new = int(re.sub(r'\D', '', str(hands)))
+                        height = card.find("div", class_="txtparam").find_next_sibling().find("span")
+                        height_new = int(re.sub(r'\D', '', str(height)))
+                        weight = card.find("div", class_="txtparam").find_next_sibling().find("span").find_next_sibling().find_next_sibling()
+                        weight_new = int(re.sub(r'\D', '', str(weight)))
+                        al = card.find("span", class_="stAuthor").text
+                        price = card.find("span", class_="txtpricehour").find_next_sibling().text
+
+                        try:
+                            price_new = "".join(price[:6].split())
+                            if price_new[-1] != '0':
+                                price_new = price_new[:-1]
+                            price_new = int(price_new)
+                        except Exception as _ex:
+                            print(_ex)
+                            price_new = 0
+                        
+                        val = (id_new, place, name_new, age_new, hands_new, height_new, weight_new, al, price_new)
+
+                        try:
+                            cur.execute("INSERT INTO people VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)", val)
+                        
+                        except Exception as _ex:
+                            print(_ex)
+
+            except Exception as _ex:
+                print(_ex)
+
+            finally:
+                driver.close()
+                driver.quit()
+
+
+    create_db()
+    get_source_html(search_url)
+
+
+@bot.message_handler(commands=['email'])
+def task_completed(message):
+    bot.send_message(message.chat.id, 'Введите small, middle или big ')
+    bot.register_next_step_handler(message, search)
+
+
+def search(message):
+    db_1 = "people.db"
+    search_1 = os.getenv('SEARCH')
+    profiles = []
+    places_list = []
+    
+
+    def send_email(email_data):
+        sender = os.getenv("EMAIL")
+        recipient = os.getenv("EMAIL_RECIPIENT")
+        password = os.getenv("EMAIL_PASSWORD")
+        server = smtplib.SMTP("smtp.rambler.ru", 587)
+        server.starttls()
+        msg = MIMEText(str(email_data))
+        msg["Subject"] = "Test"
+
+        try:
+            # time.sleep(random.randint(0,3))
+            server.login(sender, password)
+            server.sendmail(sender, recipient, msg.as_string())
+        except Exception as _ex:
+            print(f"{_ex}")
+
+    def random_output(data):
+        for result in random.sample(data, 5):
+            send_email(result)
+
+
+    def places_output(distance):
+        with sq.connect(db_1) as con:
+            cur = con.cursor()
+            cur.execute("SELECT place FROM places WHERE (distance BETWEEN 1 AND ?)", (distance,))
+            for row in cur:
+                places_list.append(row[0])
+
+    
+    def filter(search_data, visit_anks):
+        with sq.connect(db_1) as con:
+            cur = con.cursor()
+            cur.execute(search_1)
+            if search_data == 'small':
+                places_output(1)
+            if search_data == 'middle':
+                places_output(2)
+            if search_data == 'big':
+                places_output(3)
+            for result in cur:
+                if (result[1] in places_list) and (result[0] not in visit_anks): 
+                    profiles.append(result)
+        random_output(profiles)
+
+
+    def visits():
+        visit_anks = []
+        with sq.connect(db_1) as con:
+            cur = con.cursor()
+            cur.execute("SELECT number_profile FROM profiles")
+            for row in cur:
+                visit_anks.append(row[0])
+        return visit_anks
+
+
+    filter(message.text, visits())
+
 
 # @bot.message_handler(commands=['reminder'])
 # def reminder_message(message):
